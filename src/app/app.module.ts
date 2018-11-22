58c4a9097eb26e890690c1f91399119f517a2add
Malgorzata Zawisza <malgorzata.zawisza@globallogic.com>
2018-11-22 11:03:44 +0100

    Initial commit, but project already done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodoFormComponent } from './home/todo-form/todo-form.component';
+import { TodoListComponent } from './home/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodoFormComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
